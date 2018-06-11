1c35e820f458f0cedad4d2f04374a16d043e814d
Zebediah Figura <z.figura12@gmail.com>
2018-06-10 23:12:16 -0500

    server: Allocate shared memory segments for semaphores and mutexes.
    As has been described in the README, these two objects have state that can't
    be expressed (or read from) the eventfd descriptor. Namely, for semaphores
    we need to know the current lock count, and for mutexes we need to know the
     owner tid and the recursion count. To make these work across processes, we
    store them in a global shared memory section.
    
    This patch has no effect by itself; it's for separation.
    
    We use the WINEPREFIX dir to discriminate shm sections for simultaneously
    running servers; this is based off of code in libwine (specifically
    init_server_dir()).

--- include/wine/server_protocol.h
+++ include/wine/server_protocol.h
@@ -5644,6 +5644,8 @@ struct create_esync_reply
     struct reply_header __header;
     obj_handle_t handle;
     int          type;
+    unsigned int shm_idx;
+    char __pad_20[4];
 };
 
 
@@ -5662,6 +5664,8 @@ struct open_esync_reply
     struct reply_header __header;
     obj_handle_t handle;
     int          type;
+    unsigned int shm_idx;
+    char __pad_20[4];
 };
 
 
@@ -6581,6 +6585,6 @@ union generic_reply
     struct get_esync_fd_reply get_esync_fd_reply;
 };
 
-#define SERVER_PROTOCOL_VERSION 559
+#define SERVER_PROTOCOL_VERSION 560
 
 #endif /* __WINE_WINE_SERVER_PROTOCOL_H */
