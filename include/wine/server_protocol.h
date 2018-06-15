f41f355859339345da586fd3cde9bde7e268f412
Zebediah Figura <z.figura12@gmail.com>
2018-06-15 11:01:44 -0500

    server: Create eventfd descriptors for timers.

--- include/wine/server_protocol.h
+++ include/wine/server_protocol.h
@@ -5683,6 +5683,7 @@ enum esync_type
     ESYNC_AUTO_EVENT,
     ESYNC_MANUAL_EVENT,
     ESYNC_MUTEX,
+    ESYNC_AUTO_SERVER,
     ESYNC_MANUAL_SERVER,
     ESYNC_QUEUE,
 };
@@ -6580,6 +6581,6 @@ union generic_reply
     struct get_esync_fd_reply get_esync_fd_reply;
 };
 
-#define SERVER_PROTOCOL_VERSION 558
+#define SERVER_PROTOCOL_VERSION 559
 
 #endif /* __WINE_WINE_SERVER_PROTOCOL_H */
